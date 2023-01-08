9a6218e4241a7f3c7ce2f372e9fbeb15db92f553
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-08 16:56:39 +0500

    I worked until 85

---
+++ Day#002/index.js
@@ -0,0 +1,61 @@
+// Counting Cards
+// let count = 0;
+// function cc(card) {
+//   if (card === 2 || card === 3 || card === 4 || card === 5 || card === 6)
+//     count++ + " Bet";
+//   if (card === 7 || card === 8 || card === 9) count += 0;
+//   if (
+//     card === 10 ||
+//     card === "J" ||
+//     card === "Q" ||
+//     card === "K" ||
+//     card === "A"
+//   )
+//     count-- + " Hold";
+//   if (count > 0) return count + " Bet";
+//   else return count + " Hold";
+// }
+
+// console.log(cc(2));
+// console.log(cc(3));
+// console.log(cc(4));
+// console.log(cc(5));
+// console.log(cc(6));
+
+// Build JavaScript Objects
+// const myDog = {
+//   name: "Whiskers",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["one", "two"],
+// };
+
+// Accessing Object Properties with Dot Notation
+// const testObj = {
+//   hat: "ballcap",
+//   shirt: "jersey",
+//   shoes: "cleats",
+// };
+
+// const hatValue = testObj.hat;
+// const shirtValue = testObj.shirt;
+
+// Accessing Object Properties with Bracket Notation
+// const testObj = {
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water",
+// };
+
+// const entreeValue = testObj["an entree"];
+// const drinkValue = testObj["the drink"];
+
+// Accessing Object Properties with Variables
+// const testObj = {
+//   12: "Namath",
+//   16: "Montana",
+//   19: "Unitas",
+// };
+
+// const playerNumber = 16;
+// const player = testObj[playerNumber];
