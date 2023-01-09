c544176ff5a635e32e6fb5788426d8fca02853b6
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-09 23:04:57 +0500

    I worked until 93

---
+++ Day#003/index.js
@@ -0,0 +1,97 @@
+// Updating Object Properties
+// const myDog = {
+//   name: "Coder",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["freeCodeCamp Campers"],
+// };
+
+// myDog.name = "Happy Coder";
+
+// Add New Properties to a JavaScript Object
+// const myDog = {
+//   name: "Happy Coder",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["freeCodeCamp Campers"],
+// };
+// myDog.bark = "woof";
+
+// Delete Properties from a JavaScript Object
+// const myDog = {
+//   name: "Happy Coder",
+//   legs: 4,
+//   tails: 1,
+//   friends: ["freeCodeCamp Campers"],
+//   bark: "woof",
+// };
+// delete myDog.tails;
+
+// Using Objects for Lookups
+// function phoneticLookup(val) {
+//   const obj = {
+//     alpha: "Adams",
+//     bravo: "Boston",
+//     charlie: "Chicago",
+//     delta: "Denver",
+//     echo: "Easy",
+//     foxtrot: "Frank",
+//   };
+//   let result = obj[val];
+//   return result;
+// }
+
+// console.log(phoneticLookup(""));
+
+// Testing Objects for Properties
+// function checkObj(obj, checkProp) {
+//   return obj[checkProp] ? obj[checkProp] : "Not Found";
+// }
+// console.log(checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" }, "house"));
+
+// Manipulating Complex Objects
+// const myMusic = [
+//   {
+//     artist: "Billy Joel",
+//     title: "Piano Man",
+//     release_year: 1973,
+//     formats: ["CD", "8T", "LP"],
+//     gold: true,
+//   },
+//   {
+//     artist: "John Doe",
+//     title: "Actor",
+//     release_year: 1973,
+//     formats: ["CD", "8T", "LP"],
+//     gold: true,
+//   },
+// ];
+
+// Accessing Nested Objects
+// const myStorage = {
+//   car: {
+//     inside: {
+//       "glove box": "maps",
+//       "passenger seat": "crumbs",
+//     },
+//     outside: {
+//       trunk: "jack",
+//     },
+//   },
+// };
+
+// const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// Accessing Nested Arrays
+// const myPlants = [
+//   {
+//     type: "flowers",
+//     list: ["rose", "tulip", "dandelion"],
+//   },
+//   {
+//     type: "trees",
+//     list: ["fir", "pine", "birch"],
+//   },
+// ];
+
+// const secondTree = myPlants[1].list[1];
