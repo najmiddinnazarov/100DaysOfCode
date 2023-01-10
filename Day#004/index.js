6248896aef47452f18ea39e418b132f30bd11226
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-10 19:23:53 +0500

    I worked until 100

---
+++ Day#004/index.js
@@ -0,0 +1,87 @@
+// Record Collection
+// const recordCollection = {
+//   2548: {
+//     albumTitle: "Slippery When Wet",
+//     artist: "Bon Jovi",
+//     tracks: ["Let It Rock", "You Give Love a Bad Name"],
+//   },
+//   2468: {
+//     albumTitle: "1999",
+//     artist: "Prince",
+//     tracks: ["1999", "Little Red Corvette"],
+//   },
+//   1245: {
+//     artist: "Robert Palmer",
+//     tracks: [],
+//   },
+//   5439: {
+//     albumTitle: "ABBA Gold",
+//   },
+// };
+
+// function updateRecords(records, id, prop, value) {
+//   if (prop !== " ") records[id][prop] = value;
+//   return records;
+// }
+// updateRecords(recordCollection, 5439, "artist", "ABBA");
+
+// Iterate with JavaScript While Loops
+// const myArray = [];
+// let i = 5;
+// while (i >= 0) {
+//   myArray.push(i);
+//   i--;
+// }
+// console.log(myArray);
+
+// Iterate with JavaScript For Loops
+// const myArray = [];
+// for (let i = 1; i <= 5; i++) {
+//   myArray.push(i);
+// }
+
+// Iterate Odd Numbers With a For Loop
+// const myArray = [];
+// for (let i = 1; i < 10; i += 2) {
+//   myArray.push(i);
+// }
+
+// Count Backwards With a For Loop
+// const myArray = [];
+// for (let i = 9; i > 0; i -= 2) {
+//   myArray.push(i);
+// }
+
+// Iterate Through an Array with a For Loop
+// const myArr = [2, 3, 4, 5, 6];
+// let total = 0;
+// for (let i = 0; i < myArr.length; i++) {
+//   total += myArr[i];
+// }
+
+// Nesting For Loops
+// function multiplyAll(arr) {
+//   let product = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       product *= arr[i][j];
+//     }
+//   }
+//   return product;
+// }
+
+// console.log(
+//   multiplyAll([
+//     [1, 2],
+//     [3, 4],
+//     [5, 6, 7],
+//   ])
+// );
+
+// Iterate with JavaScript Do...While Loops
+// const myArray = [];
+// let i = 10;
+// do {
+//   myArray.push(i);
+//   i++;
+// } while (i < 11);
