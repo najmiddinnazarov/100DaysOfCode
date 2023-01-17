16e1d005567b723763bc202061d1d1a88eea4ee2
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-17 16:19:29 +0500

    I worked until 13

---
+++ Day#011/index.js
@@ -0,0 +1,14 @@
+// Match Single Characters Not Specified
+/* let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/ig;
+let result = quoteSample.match(myRegex); */
+
+// Match Characters that Occur One or More Times
+/* let difficultSpelling = "Mississippi";
+let myRegex = /s+/g;
+let result = difficultSpelling.match(myRegex);
+ */
+
+// Match Characters that Occur Zero or More Times
+// let chewieRegex = /Aa*/;
+// let result = chewieQuote.match(chewieRegex);
