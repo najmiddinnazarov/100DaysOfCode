7879c8e0b9cef14c86c3151bb00fcfd91cc11c29
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-21 20:59:32 +0500

    I worked until 24

---
+++ Day#013/index.js
@@ -0,0 +1,14 @@
+// Restrict Possible Usernames
+/* let username = "JackOfAllTrades";
+const userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i;
+let result = userCheck.test(username); */
+
+// Match Whitespace
+/* let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; 
+let result = sample.match(countWhiteSpace); */
+
+// Match Non-Whitespace Characters
+/* let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g;
+let result = sample.match(countNonWhiteSpace); */
