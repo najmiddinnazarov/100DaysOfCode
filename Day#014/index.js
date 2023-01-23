69a91fa272f1493548f0c53fe7683c5947399bd8
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-23 23:32:13 +0500

    I worked until 28

---
+++ Day#014/index.js
@@ -0,0 +1,19 @@
+// Specify Upper and Lower Number of Matches
+/* let ohStr = "Ohhh no";
+let ohRegex = /Oh{3,6}\sno/;
+let result = ohRegex.test(ohStr); */
+
+// Specify Only the Lower Number of Matches
+/* let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/;
+let result = haRegex.test(haStr); */
+
+// Specify Exact Number of Matches
+/* let timStr = "Timmmmber";
+let timRegex = /Tim{4}ber/;
+let result = timRegex.test(timStr); */
+
+// Check for All or None
+/* let favWord = "favorite";
+let favRegex = /favou?rite/;
+let result = favRegex.test(favWord); */
