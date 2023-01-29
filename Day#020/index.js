5c07c778bc024efb63a8cc765513d82573b4c82e
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-01-29 20:21:23 +0500

    I worked until 7

---
+++ Day#020/index.js
@@ -0,0 +1,31 @@
+// Return Largest Numbers in Arrays
+/* function largestOfFour(arr) {
+  return arr.map((val) => {
+    return Math.max(...val);
+  });
+}
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1],
+  ])
+);
+ */
+
+// Confirm the Ending
+/* function confirmEnding(str, target) {
+  return str.split("").splice(-target.length).join("") === target;
+}
+console.log(confirmEnding("Open sesame", "same")); */
+
+// Repeat a String Repeat a String
+/* function repeatStringNumTimes(str, num) {
+  let array = [];
+  for (let i = 0; i < num; i++) {
+    array.push(str);
+  }
+  return array.join("");
+}
+repeatStringNumTimes("abc", 3); */
