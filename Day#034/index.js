76354ede68828e37754bc9d9300e5763f2ab2558
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-13 21:38:31 +0500

    Intermediate Algorithm Scripting. I worked until 2

---
+++ Day#034/index.js
@@ -0,0 +1,20 @@
+/* =========== Start Intermediate Algorithm Scripting  ===========  */
+
+// Sum All Numbers in a Range
+/* function sumAll(arr) {
+  let count = 0;
+  for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+    count += i;
+  }
+  return count;
+}
+console.log(sumAll([1, 4])); */
+
+// Diff Two Arrays
+/* function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+ */
