dbd0302c9762aba041aa0e16fc444a63360be5ce
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-14 22:05:19 +0500

    I worked until 3

---
+++ Day#035/index.js
@@ -0,0 +1,6 @@
+// Seek and Destroy
+/* function destroyer(arr, ...valsToRemove) {
+  return arr.filter((elem) => !valsToRemove.includes(elem));
+}
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+ */
