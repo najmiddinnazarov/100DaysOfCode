62ffb9b700e0d60a35d1ae06a09744f407dde8de
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-17 22:22:23 +0500

    I worked until 6

---
+++ Day#038/index.js
@@ -0,0 +1,9 @@
+// Pig Latin
+/* function translatePigLatin(str) {
+  let consonantRegex = /^[^aeiou]+/;
+  let myConsonants = str.match(consonantRegex);
+  return myConsonants !== null
+    ? str.replace(consonantRegex, "").concat(myConsonants).concat("ay")
+    : str.concat("way");
+}
+translatePigLatin("consonant"); */
