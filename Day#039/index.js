91b7c0cfea851c8862ac7b2e0e73c59db99dca35
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-18 20:23:26 +0500

    I worked until 7

---
+++ Day#039/index.js
@@ -0,0 +1,12 @@
+// Search and Replace
+/* function myReplace(str, before, after) {
+  let find = str.match(before);
+  if (/[A-Z]/.test(find)) {
+    let upper = after.charAt(0).toUpperCase() + after.slice(1);
+    return str.replace(before, upper);
+  } else {
+    let lower = after.charAt(0).toLowerCase() + after.slice(1);
+    return str.replace(before, lower);
+  }
+}
+console.log(myReplace("I think we should look up there", "up", "Down")); */
