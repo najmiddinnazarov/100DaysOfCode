4a9e92edf0d21891e5f87fa56391c9889c28c58b
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-19 20:43:03 +0500

    I worked until 8

---
+++ Day#040/index.js
@@ -0,0 +1,11 @@
+// DNA Pairing
+/* function pairElement(str) {
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C",
+  };
+  return str.split("").map((x) => [x, pairs[x]]);
+}
+pairElement("GCG"); */
