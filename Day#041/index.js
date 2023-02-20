850c312a37567866bdd262c9940fde46823a5b09
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-20 21:25:42 +0500

    I worked until 9

---
+++ Day#041/index.js
@@ -0,0 +1,10 @@
+// Missing letters
+/* function fearNotLetter(str) {
+  for (let i = 1; i < str.length; ++i) {
+    if (str.charCodeAt(i) - str.charCodeAt(i - 1) > 1) {
+      return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+    }
+  }
+}
+fearNotLetter("abce");
+ */
