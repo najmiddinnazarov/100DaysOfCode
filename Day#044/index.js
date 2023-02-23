48485e70aa610894140654ac2e5bb592909e9339
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-23 23:22:34 +0500

    I worked until 13

---
+++ Day#044/index.js
@@ -0,0 +1,10 @@
+// Sum All Primes
+/* function sumPrimes(num) {
+  let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0)) primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+}
+console.log(sumPrimes(10));
+ */
