447d18e4ce8d3b4123d2ef51172643b7a18c9d84
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-02-26 18:49:37 +0500

    JavaScript Algorithms and Data Structures Projects. I worked until 1

---
+++ Day#047/index.js
@@ -0,0 +1,10 @@
+/* ================ Start JavaScript Algorithms and Data Structures Projects  ================ */
+
+// Palindrome Checker
+/* function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, "");
+  var reverseStr = lowRegStr.split("").reverse().join("");
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama"); */
