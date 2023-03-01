9088195b6ddc3af303dcce8b5a191f4e1aa9eff5
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-01 21:13:41 +0500

    912. Sort an Array

---
+++ Day#050/index.js
@@ -0,0 +1,8 @@
+/* =========== Start Leetcode problems  ===========  */
+
+// 912. Sort an Array
+/* var sortArray = function (nums) {
+  return nums.sort((a, b) => a - b);
+};
+console.log(sortArray([5, 2, 3, 1]));
+ */
