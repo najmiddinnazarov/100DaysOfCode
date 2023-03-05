e1877ca1be79134741ad6e0701d286c0154f4100
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-05 23:08:38 +0500

    58. Length of Last Word

---
+++ Day#054/index.js
@@ -0,0 +1,7 @@
+// 58. Length of Last Word
+/* var lengthOfLastWord = function (s) {
+  let arr = s.trim().split(" ");
+  return arr[arr.length - 1].length;
+};
+console.log(lengthOfLastWord("   fly me   to   the moon  "));
+ */
