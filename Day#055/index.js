2ae71564dbc16602c81b723112138e03e84cd956
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-06 22:27:05 +0500

    1108. Defanging an IP Address

---
+++ Day#055/index.js
@@ -0,0 +1,15 @@
+// 2469. Convert the Temperature
+/* var convertTemperature = function (celsius) {
+  let kelvin = celsius + 273.15;
+  let fahrenheit = celsius * 1.8 + 32.0;
+  return [kelvin, fahrenheit];
+};
+console.log(convertTemperature(36.5));
+ */
+
+// 1108. Defanging an IP Address
+/* var defangIPaddr = function (address) {
+  return address.replace(/[.]/g, "[.]");
+};
+console.log(defangIPaddr("255.100.50.0"));
+ */
