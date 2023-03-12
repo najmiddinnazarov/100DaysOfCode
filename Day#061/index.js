8a34181c8ede1e12a27efa5b536f43bcbac3fe8b
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-12 23:14:11 +0500

    1859. Sorting the Sentence

---
+++ Day#061/index.js
@@ -0,0 +1,12 @@
+// 1859. Sorting the Sentence
+/* var sortSentence = function (s) {
+  const wordArray = s.split(" ");
+  const orderedSentence = [];
+  for (let i = 0; i < wordArray.length; i++) {
+    const idx = wordArray[i].slice(-1) - 1;
+    orderedSentence[idx] = wordArray[i].slice(0, -1);
+  }
+  return orderedSentence.join(" ");
+};
+console.log(sortSentence("is2 sentence4 This1 a3"));
+ */
