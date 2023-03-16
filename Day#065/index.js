98b62bf40f457f05444e33afea868bca149639fa
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-16 23:24:34 +0500

    1929. Concatenation of Array

---
+++ Day#065/index.js
@@ -0,0 +1,18 @@
+// 1920. Build Array from Permutation
+/* var buildArray = function (nums) {
+  return nums.map((a) => nums[a]);
+};
+console.log(buildArray([5, 0, 1, 2, 3, 4]));
+ */
+
+// 1929. Concatenation of Array
+/* var getConcatenation = function (nums) {
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = nums[i];
+    result[i + nums.length] = nums[i];
+  }
+  return result;
+};
+console.log(getConcatenation([1, 2, 1]));
+ */
