2b9e554176498d68579ecf9d5056cdb8e8bc3d32
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-20 21:36:22 +0500

    2586. Count the Number of Vowel Strings in Range

---
+++ Day#069/index.js
@@ -0,0 +1,16 @@
+// 2586. Count the Number of Vowel Strings in Range
+/* var vowelStrings = function (words, left, right) {
+  let result = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let i = left; i <= right; i++) {
+    if (
+      vowels.includes(words[i][0]) &&
+      vowels.includes(words[i][words[i].length - 1])
+    ) {
+      result++;
+    }
+  }
+  return result;
+};
+console.log(vowelStrings(["hey", "aeo", "mu", "ooo", "artro"], 1, 4));
+ */
