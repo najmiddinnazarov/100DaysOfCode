25848590298474f0afe429c115b9d170945e53d4
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-22 22:46:12 +0500

    136. Single Number

---
+++ Day#071/index.js
@@ -0,0 +1,7 @@
+// 136. Single Number
+/* var singleNumber = function (nums) {
+  let dublicateNum = nums.filter((vl, index) => nums.indexOf(vl) !== index);
+  return nums.filter((vl) => !dublicateNum.includes(vl));
+};
+console.log(singleNumber([4, 1, 2, 1, 2]));
+ */
