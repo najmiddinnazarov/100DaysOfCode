e9422e31e2d74c9baa9ca8fbe5c7807bcd321f77
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-23 23:41:59 +0500

    26. Remove Duplicates from Sorted Array

---
+++ Day#072/index.js
@@ -0,0 +1,11 @@
+// 26. Remove Duplicates from Sorted Array
+/* const removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; ) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+};
+console.log(removeDuplicates([1, 1, 2])); */
