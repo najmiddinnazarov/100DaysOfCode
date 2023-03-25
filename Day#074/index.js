158cfb7e0f16d32f00bdd3db42769fef9237212e
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-25 22:01:09 +0500

    2011. Final Value of Variable After Performing Operations

---
+++ Day#074/index.js
@@ -0,0 +1,5 @@
+// 2011. Final Value of Variable After Performing Operations
+/* var finalValueAfterOperations = function (operations) {
+  return operations.reduce((acc, curr) => (curr[1] === "+" ? ++acc : --acc), 0);
+};
+console.log(finalValueAfterOperations(["++X", "++X", "X++"])); */
