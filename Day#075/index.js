f2568cd7ba855e4b96c988c67a66c14613a6ec8f
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-26 21:47:39 +0500

    2413. Smallest Even Multiple

---
+++ Day#075/index.js
@@ -0,0 +1,8 @@
+// 2413. Smallest Even Multiple
+/* var smallestEvenMultiple = function (n) {
+  let result = 1;
+  while (result % 2 !== 0 || result % n !== 0) result++;
+  return result;
+};
+console.log(smallestEvenMultiple(5));
+ */
