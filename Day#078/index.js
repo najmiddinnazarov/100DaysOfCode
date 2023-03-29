402b4201bcc905fc69b4ecf894a9afc3741ded9e
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-03-29 22:29:16 +0500

    242. Valid Anagram

---
+++ Day#078/index.js
@@ -0,0 +1,10 @@
+// 242. Valid Anagram
+/* var isAnagram = function (s, t) {
+  s = s.split("").sort();
+  t = t.split("").sort();
+  if (s.length !== t.length) return false;
+  for (var i = 0; i < s.length; i++) if (s[i] !== t[i]) return false;
+  return true;
+};
+console.log(isAnagram("anagram", "nagaram"));
+ */
