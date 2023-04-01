1bcd57e7f8c80874ddda0bec394678643a0cc9e9
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-01 23:37:08 +0500

    // 283. Move Zeroes

---
+++ Day#081/index.js
@@ -0,0 +1,20 @@
+// 283. Move Zeroes
+/* let moveZeroes = function (nums) {
+  let low = 0;
+  let high = low + 1;
+  while (high <= nums.length - 1) {
+    if (nums[low] !== 0) {
+      low++;
+      high++;
+    } else {
+      if (nums[high] !== 0) {
+        [nums[low], nums[high]] = [nums[high], nums[low]];
+        low++;
+      }
+      high++;
+    }
+  }
+  return nums;
+};
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+ */
