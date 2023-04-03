0bb800553aa4e6b7012b0d31f633187d3859acaa
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-03 23:26:14 +0500

    94. Binary Tree Inorder Traversal

---
+++ Day#083/index.js
@@ -0,0 +1,11 @@
+// 94. Binary Tree Inorder Traversal
+/* var inorderTraversal = function (root) {
+  if (root === null) return [];
+  return [
+    ...inorderTraversal(root.left),
+    root.val,
+    ...inorderTraversal(root.right),
+  ];
+};
+console.log([1, null, 2, 3]);
+ */
