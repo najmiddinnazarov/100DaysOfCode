5a3b7688c7071dbfd18f016b15a8b5c783036f39
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-04 21:50:25 +0500

    206. Reverse Linked List

---
+++ Day#084/index.js
@@ -0,0 +1,14 @@
+// 206. Reverse Linked List
+/* var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
+console.log(reverseList([1, 2, 3, 4, 5])); */
