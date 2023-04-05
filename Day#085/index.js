1544ff493689bc85dad6fa2ca3f7d5601c5675a1
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-05 22:15:07 +0500

    202. Happy Number

---
+++ Day#085/index.js
@@ -0,0 +1,8 @@
+// 202. Happy Number
+/* function isHappy(n, sum = 0, n2 = 10) {
+  if (n === 1) return true;
+  if (n2 === 0) return false;
+  for (let c of n + "") sum += +c * +c;
+  return isHappy(sum, 0, n2 - 1);
+}
+console.log(isHappy(19)); */
