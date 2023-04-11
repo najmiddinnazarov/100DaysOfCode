a169b1f810f601a53e47a63e5646da296fdd0ee4
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-11 23:59:56 +0500

    88. Merge Sorted Array

---
+++ Day#091/index.js
@@ -0,0 +1,9 @@
+// 88. Merge Sorted Array
+/* var merge = function (num1, m, num2, n) {
+  let j = 0;
+  for (let i = m; i < m + n; i++) {
+    num1[i] = num2[j];
+    j++;
+  }
+  console.log(num1.sort((a, b) => a - b));
+}; */
