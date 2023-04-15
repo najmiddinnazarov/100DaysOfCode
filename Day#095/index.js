6af3ce494db01f039339eca45a050d3c70fdc5fd
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-15 22:24:48 +0500

    191. Number of 1 Bits

---
+++ Day#095/index.js
@@ -0,0 +1,6 @@
+// 191. Number of 1 Bits
+/* var hammingWeight = function (int) {
+  return int.toString(2).replaceAll("0", "").length;
+};
+console.log(hammingWeight(00000000000000000000000000001011));
+ */
