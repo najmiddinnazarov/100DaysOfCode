ec339d9095d3ce7fe9390e8fe37d3441006ec1d4
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-16 23:45:18 +0500

    389. Find the Difference

---
+++ Day#096/index.js
@@ -0,0 +1,8 @@
+// 389. Find the Difference
+/* var findTheDifference = function (s, t) {
+  const sum1 = s.split("").reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  const sum2 = t.split("").reduce((acc, cur) => acc + cur.charCodeAt(0), 0);
+  return String.fromCharCode(sum2 - sum1);
+};
+console.log(findTheDifference("abcd", "abcde"));
+ */
