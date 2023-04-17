c229306be6a4ad978e11046b4db6a136dd2ab327
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-17 22:12:31 +0500

    1431. Kids With the Greatest Number of Candies

---
+++ Day#097/index.js
@@ -0,0 +1,12 @@
+// 1431. Kids With the Greatest Number of Candies
+/* var kidsWithCandies = function (candies, extraCandies) {
+  let result = [];
+  for (let i = 0; i < candies.length; i++) {
+    candies[i] + extraCandies >= Math.max(...candies)
+      ? result.push(true)
+      : result.push(false);
+  }
+  return result;
+};
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+ */
