0ecc43efb8466d393f67c47aa748995c7c6baa64
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2023-04-20 23:43:41 +0500

    2635. Apply Transform Over Each Element in Array

---
+++ Day#100/index.js
@@ -0,0 +1,13 @@
+// 2635. Apply Transform Over Each Element in Array
+/* var map = function (arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
+console.log(
+  map([1, 2, 3], function plusone(n) {
+    return n + 1;
+  })
+);
+ */
